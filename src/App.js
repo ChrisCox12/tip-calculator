c2fd1f9eb368ea50d3e5d27774007b7ffd458c24
ChrisCox12 <chriscox9856@gmail.com>
2021-11-23 18:56:44 -0500

    added javascript functionality to get the calculator working; added a little to the html, css, and javascript for zero error

--- src/App.js
+++ src/App.js
@@ -2,9 +2,84 @@ import logo from './assets/logo.svg';
 import dollar from './assets/icon-dollar.svg';
 import person from './assets/icon-person.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [bill_amount, setBillAmount] = useState(0);
+  const [tip_amount, setTipAmount] = useState(0);
+  const [num_people, setNumPeople] = useState(0);
+  const [total_per_person, setTotalPerPerson] = useState(0);
+  const [tip_per_person, setTipPerPerson] = useState(0);
+
+  function setBill(event){
+    // For some reason, the bill amount was of type string, 
+    // so I had to parse to a number, in this case a Float
+    setBillAmount( parseFloat(event.target.value, 10) );
+  }
+
+  function setTip_5() {
+    setTipAmount(0.05);
+  }
+
+  function setTip_10() {
+    setTipAmount(0.10);
+  }
+
+  function setTip_15() {
+    setTipAmount(0.15);
+  }
+
+  function setTip_25() {
+    setTipAmount(0.25);
+  }
+
+  function setTip_50() {
+    setTipAmount(0.50);
+  }
+
+  function setTip_Custom(event) {
+    setTipAmount(event.target.value);
+  }
+
+  function setPeople(event) {
+    // For some reason, the input for the number of people was of type string, 
+    // so I had to parse to a number (in this case, an Integer)
+    setNumPeople( parseInt(event.target.value, 10) );
+  }
+
+  function reset() {
+    setBillAmount(0);
+    setTipAmount(0);
+    setNumPeople(0);
+    setTotalPerPerson(0);
+    setTipPerPerson(0);    
+  }
+
+  useEffect(() => {
+    updateAmounts();
+  })
+
+  function updateAmounts() {
+    if(num_people === 0){
+      let error = document.getElementById("zero-error");
+
+      error.classList.remove("noshow");
+      error.classList.add("show");
+    }
+    else {
+      let tip = (bill_amount * tip_amount) / num_people;
+      let total = ((bill_amount * tip_amount) + bill_amount) / num_people;
+
+      /* let string = "Bill: " + bill_amount + "\nTip Amount: " + tip_amount + "\nPeople: " + num_people + "\nTip / person: " + tip_per_person + "\nTotal / person: " + total_per_person;
+      console.log(string);
+      console.log("\nTotal: " + total);
+      console.log("\nType of people: " + typeof num_people); */
+
+      setTipPerPerson(tip);
+      setTotalPerPerson(total);
+    }
+  }
+
   return (
     <div className="App">
       <div className="App__logo"><img src={logo} alt="The logo for the app, Spliter" /></div>
@@ -13,7 +88,8 @@ function App() {
           <div className="calculator__bill">
             <label htmlFor="bill">Bill</label> <br />
             {/* step property below allows for decimals to be considered valid */}
-            <input type="number" name="bill" id="bill" min="0" placeholder="0" step="0.01" />
+            <input type="number" name="bill" id="bill" value={bill_amount} onChange={setBill} min="0" placeholder="0" step="0.01" />
+            {/* <h3>{bill_amount}</h3> */}
             <div className="input-icon icon--dollar">
               <img src={dollar} alt="Icon of a dollar sign" />
             </div>
@@ -21,34 +97,42 @@ function App() {
           <div className="calculator__tip-selection">
             <label htmlFor="custom">Select Tip %</label> <br />
             <div className="tip-selections grid grid--tip">
-              <div className="btn btn--tip unselected">5%</div>
-              <div className="btn btn--tip unselected">10%</div>
-              <div className="btn btn--tip unselected">15%</div>
-              <div className="btn btn--tip unselected">25%</div>
-              <div className="btn btn--tip unselected">50%</div>
-              <input type="number" name="custom" id="custom" placeholder="Custom" />
+              <div className="btn btn--tip unselected" onClick={setTip_5}>5%</div>
+              <div className="btn btn--tip unselected" onClick={setTip_10}>10%</div>
+              <div className="btn btn--tip unselected" onClick={setTip_15}>15%</div>
+              <div className="btn btn--tip unselected" onClick={setTip_25}>25%</div>
+              <div className="btn btn--tip unselected" onClick={setTip_50}>50%</div>
+              <input type="number" name="custom" id="custom" value={tip_amount} onChange={setTip_Custom} placeholder="Custom" />
+              {/* <h3>{tip_amount}</h3>
+              <h3>Tip: ({bill_amount} * {tip_amount}) / {num_people}</h3>
+              <h3>TOtal: (({bill_amount} * {tip_amount}) + {bill_amount}) / {num_people}</h3> */}
             </div>
           </div>
           <div className="calculator__people">
-            <label htmlFor="numPeople">Number of People</label> <br />
-            <input type="number" name="numPeople" id="numPeople" placeholder="0" />
+            <div className="flex s-b">
+              <label htmlFor="numPeople">Number of People</label>
+              <p className="zero-error noshow" id="zero-error">Can't be zero</p>
+            </div>
+            <br />
+            <input type="number" name="numPeople" id="numPeople" value={num_people} onChange={setPeople} placeholder="0" />
             <div className="input-icon icon--person">
               <img src={person} alt="Icon of a person" />
             </div>
+            {/* <h3>{num_people}</h3> */}
           </div>
         </div>
 
-        <div class="calculator__outputs">
+        <div className="calculator__outputs">
           <div className="calculator__results">
             <div className="tip-results grid grid--results">
               <p><span>Tip Amount </span><br /> / person</p>
-              <div className="tip-amount"></div>
+              <div className="tip-amount">$ <span className="tip-results__outputs" id="t-a1">{tip_per_person}</span></div>
   
               <p><span>Total </span><br /> / person</p>
-              <div className="total-amount"></div>
+              <div className="total-amount">$ <span className="tip-results__outputs" id="t-a2">{total_per_person}</span></div>
             </div>
   
-            <div className="btn btn--reset">Reset</div>
+            <div className="btn btn--reset" onClick={reset}>Reset</div>
           </div>
         </div>
       </div>
